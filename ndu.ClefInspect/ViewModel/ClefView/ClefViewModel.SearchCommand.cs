98ccc103950356751f339e9e7d99f6288cfa9e9f
ndu <ndu3@pm.me>
2026-05-23 13:15:38 +0200

    long awaited browing (search up/down)

---
+++ ndu.ClefInspect/ViewModel/ClefView/ClefViewModel.SearchCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace ndu.ClefInspect.ViewModel.ClefView
+{
+    public partial class ClefViewModel
+    {
+        public enum SearchDirection { Undefined, Up, Down };
+        private class SearchCommand(ClefViewModel clefViewModel) : ICommand
+        {
+            private readonly ClefViewModel clefViewModel = clefViewModel;
+
+            // no CS0067 though the event is left unused
+            public event EventHandler? CanExecuteChanged { add { } remove { } }
+
+            public bool CanExecute(object? parameter)
+            {
+                return parameter is SearchDirection;
+            }
+
+            public void Execute(object? parameter)
+            {
+                if(parameter is SearchDirection dir)
+                {
+                    clefViewModel.BrowseTo(dir, clefViewModel.TextSearch);
+                }
+            }
+        }
+    }
+}
