2b27a63e9ba64d305582298ef8a2cb2a5f5e7896
ndu <ndu3@pm.me>
2026-05-28 23:46:13 +0200

    performance optimisation: searching in filter

---
+++ ndu.ClefInspect/ViewModel/ClefView/ClefFilterViewModel.ClearSearchFilterCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ndu.ClefInspect.ViewModel.ClefView
+{
+    public partial class ClefFilterViewModel
+    {
+        public class ClearSearchFilterCommand : ICommand
+        {
+            private readonly ClefFilterViewModel _vm;
+
+            public ClearSearchFilterCommand(ClefFilterViewModel vm)
+            {
+                _vm = vm;
+                PropertyChangedEventManager.AddHandler(_vm, (s, e) => { CanExecuteChanged?.Invoke(this, EventArgs.Empty); }, nameof(vm.SearchFilter));
+            }
+
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter)
+            {
+                return _vm.SearchFilter != string.Empty;
+            }
+
+            public void Execute(object? parameter)
+            {
+                _vm.SearchFilter = string.Empty;
+            }
+        }
+
+    }
+}
