ef0d4bac990540efa6ccf89154c9064e51c1305c
ndu <ndu3@pm.me>
2026-06-02 22:25:42 +0200

    reorder open multiple files

--- ndu.ClefInspect/View/MainView.xaml.cs
+++ ndu.ClefInspect/View/MainView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using static ndu.ClefInspect.SingleInstanceManager;
 
@@ -37,8 +38,21 @@ namespace ndu.ClefInspect.View
                     ((MainViewModel)this.DataContext).OpenFiles(files);
                 }
             }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
         }
 
+        private void OnDragOver(object sender, DragEventArgs e)
+        {
+            base.OnDragOver(e);
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
 
         public double ButtonSize
         {
@@ -50,6 +64,54 @@ namespace ndu.ClefInspect.View
                         VisualTreeHelper.GetDpi(this).PixelsPerDip);
                 return formattedText.Width;
             }
+        }
+        private void FileListOnMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed &&
+                sender is TextBlock textBlockFile &&
+                DataContext is MainViewModel mainViewModel &&
+                mainViewModel.StartReorderSelectedFilesStart())
+            {
+                DragDropEffects result = DragDrop.DoDragDrop(textBlockFile, textBlockFile.Text, DragDropEffects.Move);
+                if(result.HasFlag(DragDropEffects.Move))
+                {
+                    mainViewModel.ReorderSelectedFilesConfirm();
+                }
+                else
+                {
+                    mainViewModel.CancelReorderSelectedFiles();
+                }
+            }
+        }
+        private bool FileListDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(typeof(String).FullName) is string draggedFileName &&
+                sender is TextBlock textBlockFile &&
+                DataContext is MainViewModel mainViewModel &&
+                mainViewModel.SelectedFiles != null &&
+                mainViewModel.SelectedFiles.Length >= 1)
+            {
+                bool after =  e.GetPosition(textBlockFile).Y > 0.5 * textBlockFile.ActualHeight;
+                string target = textBlockFile.Text;
+                mainViewModel.ReorderSelectedFiles(draggedFileName, target, after);
+                return true;
+            }
+            return false;
+        }
+
+        private void FileListOnDragEnter(object sender, DragEventArgs e)
+        {
+            e.Handled = FileListDrop(sender, e);
+        }
+
+        private void FileListOnDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = FileListDrop(sender, e);
+        }
+
+        private void FileListOnDrop(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
